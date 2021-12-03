1ad48dcc01ce044f3b4ae6125e353268ed3766f9
fahadshoukat <fraza6970@gmail.com>
2021-12-03 22:27:32 +0500

    createMonthsAndWeekDaysArray

---
+++ count.js
@@ -0,0 +1,25 @@
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+const weekdays = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thr",
+    "Fri",
+    "Sat"
+];
+
